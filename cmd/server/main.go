0baed8f61441fdb4fbe37b0200b9efa62649fc28
Eduardo Yago Vicent <eyagovbusiness@gmail.com>
2025-07-14 14:50:33 +0200

    Configured wire DI

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,14 +2,18 @@ package main
 
 import (
 	"context"
+	"log"
 
 	"github.com/eyagovbusiness/GSWB.Users/internal/di"
 )
 
 func main() {
-	server := di.InitializeServer()
+	server, err := di.InitializeServer()
+	if err != nil {
+		log.Fatalf("failed to initialize server: %v", err)
+	}
 
 	if err := server.Start(context.Background()); err != nil {
-		panic(err)
+		log.Fatalf("failed to start server: %v", err)
 	}
 }
